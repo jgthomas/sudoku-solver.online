8608cd5d31bca09ac61a03ebc873080de177431c
James Thomas <codeactor@protonmail.com>
2018-05-20 16:31:12 +0100

    function declarations in main file

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -23,6 +23,18 @@ typedef struct Grid {
 Grid;
 
 
+void solve_puzzle(Grid *puzzle);
+void delete_puzzle(Grid *puzzle);
+bool fill_square(Grid *puzzle, int row, int col);
+int min_row(Grid *puzzle, int row);
+int min_col(Grid *puzzle, int col);
+bool box_contains(Grid *puzzle, int min_row, int min_col, int find);
+bool col_contains(Grid *puzzle, int col, int find);
+bool row_contains(Grid *puzzle, int row, int find);
+bool num_allowed(Grid *puzzle, int row, int col, int num);
+Square *make_square(int num, bool part_of_puzzle);
+
+
 void solve_puzzle(Grid *puzzle)
 {
     int row = 0;
