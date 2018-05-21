80f7623724f0fc92198cfe9f58ce58f2f302e0f7
James Thomas <codeactor@protonmail.com>
2018-05-21 19:13:08 +0100

    function to load solution into puzzle grid

--- sudoku.js
+++ sudoku.js
@@ -1,6 +1,7 @@
 const NUM_OF_SQUARES = 81;
 const EMPTY_SQUARE = "0";
 
+const puzzle = "194526837563798214827431965452679183381245796976183452719362548648957321235814679";
 
 function buildInputPuzzle() {
     const inputPuzzle = new Array(NUM_OF_SQUARES).fill(EMPTY_SQUARE);
@@ -14,11 +15,25 @@ function buildInputPuzzle() {
 }
 
 
+function displayPuzzle(puzzle) {
+    for (let i = 0; i < puzzle.length; i++) {
+        document.getElementById(`box${i}`).value = puzzle[i];
+    }
+}
+
+
 function clearPuzzle() {
     for (let i = 0; i < NUM_OF_SQUARES; i++) {
         document.getElementById(`box${i}`).value = "";
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("show").addEventListener("click", () => {
+        displayPuzzle(puzzle);
+    });
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
