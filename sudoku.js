2a74a36b58cc7091afb30234ae6e85e4c3dd30ba
James Thomas <codeactor@protonmail.com>
2018-05-28 19:55:34 +0100

    added toggle between problem and solution

--- sudoku.js
+++ sudoku.js
@@ -1,3 +1,5 @@
+const currentPuzzle = {problem: "", solution: ""};
+
 const NUM_OF_SQUARES = 81;
 const EMPTY_SQUARE = "0";
 
@@ -25,7 +27,11 @@ function solvePuzzle(puzzle) {
 
 function displayPuzzle(puzzle) {
     for (let i = 0; i < puzzle.length; i++) {
-        document.getElementById(`box${i}`).value = puzzle[i];
+        if (puzzle[i] === "0") {
+            document.getElementById(`box${i}`).value = "";
+        } else {
+            document.getElementById(`box${i}`).value = puzzle[i];
+        }
     }
 }
 
@@ -33,8 +39,25 @@ function displayPuzzle(puzzle) {
 function clearPuzzle() {
     for (let i = 0; i < NUM_OF_SQUARES; i++) {
         document.getElementById(`box${i}`).value = "";
+        currentPuzzle.problem = "";
+        currentPuzzle.solution = "";
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("toggle");
+    const currText = toggleButton.innerText;
+    toggleButton.addEventListener("click", () => {
+        if (toggleButton.innerText === currText) {
+            displayPuzzle(currentPuzzle.problem);
+            toggleButton.innerText = "Solution";
+        } else {
+            displayPuzzle(currentPuzzle.solution);
+            toggleButton.innerText = currText;
+        }
+    });
+});
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -48,7 +71,11 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("solve").addEventListener("click", () => {
         console.log(buildInputPuzzle());
         const puzzle = buildInputPuzzle();
+        currentPuzzle.problem = puzzle;
+        console.log(currentPuzzle.problem);
         const solution = solvePuzzle(puzzle);
+        currentPuzzle.solution = solution;
+        console.log(currentPuzzle.solution);
         displayPuzzle(solution);
         console.log(solution);
     });
